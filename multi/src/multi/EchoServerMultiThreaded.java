c6108d33e2318cc84f0ca69a2d412b2cdf69c257
Dorian Terbah <dorian.terb@gmail.com>
2020-09-18 15:52:59 +0200

    First commit

---
+++ multi/src/multi/EchoServerMultiThreaded.java
@@ -0,0 +1,48 @@
+package multi;
+
+/***
+ * EchoServer
+ * Example of a TCP server
+ * Date: 10/01/04
+ * Authors:
+ */
+
+
+import java.io.*;
+import java.net.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class EchoServerMultiThreaded  {
+	
+	private synchronized List<> clients;
+	
+ 	/**
+  	* main method
+	* @param EchoServer port
+  	* 
+  	**/
+       public static void main(String args[]){ 
+        ServerSocket listenSocket;
+        
+  	if (args.length != 1) {
+          System.out.println("Usage: java EchoServer <EchoServer port>");
+          System.exit(1);
+  	}
+	try {
+		listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
+		System.out.println("Server ready..."); 
+		while (true) {
+			Socket clientSocket = listenSocket.accept();
+			System.out.println("Connexion from:" + clientSocket.getInetAddress());
+			ClientThread ct = new ClientThread(clientSocket);
+			ct.start();
+		}
+        } catch (Exception e) {
+            System.err.println("Error in EchoServer:" + e);
+        }
+      }
+  }
+
+  
